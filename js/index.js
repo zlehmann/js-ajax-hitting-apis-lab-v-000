4830a96d2a4bf4b634cdd9d69bb542e69901c833
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 14:47:38 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -14,9 +14,8 @@ function displayRepositories() {
       r =>
         '<li>' +
         r.name +
-        ' - <a href="#" data-repo="' +
-        r.name +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
+        ' - <a href="' + r.archive_url + '>Repo link</a>' +
+        ' - <a href="#" onclick="getCommits(this)">Get Commits</a></li>'
     )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
