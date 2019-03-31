8d6f4163ff3d382c5082a279cb6db2831272cad2
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 14:34:34 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 // your code here
 function getRepositories() {
   const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
+}
+
+function showRepositories() {
+  console.log(this.responseText);
+  let repoList = '<ul>';
+  for (let i = 0; this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
