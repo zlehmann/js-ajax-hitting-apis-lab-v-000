166215132a1b97b06d4326f97a3a7303876f29a8
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 15:02:09 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // your code here
 function getRepositories() {
+  let username = $('#input').serialize();
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
   req.open('GET', 'https://api.github.com/users/' + username + '/repos');
