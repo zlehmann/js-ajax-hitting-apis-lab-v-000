848902ea4e87309b6bf35592cc6b31b2e3913929
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 15:15:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -33,6 +33,7 @@ function getCommits(el) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
+  console.log(commits);
   const commitsList = `<ul>${commits
     .map(
       commit =>
