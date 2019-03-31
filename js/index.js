7217c4eeb1d596327f3c8c78988c5d36cc862d09
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 14:43:41 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 // your code here
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
 
-function showRepositories() {
+function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos
