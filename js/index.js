ae3592c024e1bfddb4863e7001015bac93d9987a
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 15:13:00 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -16,17 +16,18 @@ function displayRepositories() {
         '<li>' +
         r.name +
         ' - <a href="' + r.html_url + '">Repo link</a>' +
-        ' - <a href="#" onclick="getCommits(this)">Get Commits</a></li>'
+        ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>'
     )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
 
 function getCommits(el) {
+  let username = document.getElementById('username').value;
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
-  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.open('GET', 'https://api.github.com/repos/' + username +'/' + name + '/commits');
   req.send();
 }
 
