a8790edff1206d48726b2e5517aca7f30adc9412
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 14:39:47 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function showRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos
-    .map(r => '<li>' + r.name + ' - ' + r.archive_url + '</li>')
+    .map(r => '<li>' + r.name + ' - ' + '<a href=' + r.archive_url + '> Link </a>' + '</li>')
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
