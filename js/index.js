25c077569ae62780e87264c9b07a8f20e88da3e2
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 14:59:18 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.open('GET', 'https://api.github.com/users/' + username + '/repos');
   req.send();
 }
 
